45b226a86a0a549a3e0aa9e3291b5488bacc90b4
Reinaldo Antonio Camargo Rauch <reinaldorauch@gmail.com>
2013-11-19 05:48:39 -0200

    Retirado suporte aos widechars

--- c/xadrec.c
+++ c/xadrec.c
@@ -1,19 +1,22 @@
 /**
  * Xadrec
  */
+#define _XOPEN_SOURCE_EXTENDED 1
+
 #include <ncurses.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <uchar.h>
+#include <locale.h>
 
 #include "tabuleiro.h"
 #include "movimentos.h"
 #include "jogo.h"
 #include "menu.h"
 
-
 int main() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
     char c;
 
     // Inicia a curses
