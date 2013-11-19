170b58c106d89371ee40953c44b8b39a970b736d
Reinaldo Antonio Camargo Rauch <reinaldorauch@gmail.com>
2013-11-19 06:57:53 -0200

    Comentada função first_run_jogo, incluido header movimentos.h

--- c/jogo.h
+++ c/jogo.h
@@ -6,6 +6,78 @@ extern char get_option();
 extern void cabecalho_jogo();
 
 #include "view.h"
+#include "movimentos.h"
+
+/**
+ * Captura os nomes dos jogadores
+ */
+void first_run_jogo(WINDOW *w, JOGADOR *j) {
+
+    // int *lby4 = malloc(sizeof(int)),
+    //     *wmaxx = malloc(sizeof(int)),
+    //     *wmaxy = malloc(sizeof(int)),
+    //     *lp = malloc(sizeof(int)),
+    //     *i = malloc(sizeof(int));
+
+    // char nome[TAM_NOME];
+
+    // *lby4 = LINES/4;
+
+    // getmaxyx(w, *wmaxy, *wmaxy);
+
+    // *lp = 2;
+
+    // wmove(w, (*lby4) - 1, 0);
+    // wclrtobot(w);
+    // box(w, 0 , 0);
+
+
+    // mvwaddstr(w, *lby4, *lp, "Insira o nome do jogador: BRANCAS:");
+    // curs_set(true);
+    // wmove(w, *lby4+1, *lp);
+    // wrefresh(w);
+    // for(*i = 0; *i < TAM_NOME; (*i)++) {
+    //     nome[*i] = getch();
+    //     if(nome[*i] == '\n') {
+    //         nome[*i] = '\0';
+    //         break;
+    //     }
+    // }
+
+    // strcpy(nome, j->nome);
+    
+    // curs_set(false);
+    // noecho();
+
+    // wmove(w, *lby4-1, 0);
+    // wclrtobot(w);
+    // box(w, 0 , 0);
+
+    // mvwaddstr(w, *lby4, *lp, "Insira o nome do jogador: PRETAS:");
+    // curs_set(true);
+    // wmove(w, *lby4+1, *lp);
+    // wrefresh(w);
+    // for(*i = 0; *i < TAM_NOME; (*i)++) {
+    //     nome[*i] = getch();
+    //     if(nome[*i] == '\n') {
+    //         nome[*i] = '\0';
+    //         break;
+    //     }
+    // }
+    // strcpy(nome, (j+1)->nome);
+
+    // wmove(w, *lby4-1, 0);
+    // wclrtobot(w);
+    // curs_set(false);
+    // box(w, 0 , 0);
+
+    // free(lby4);
+    // free(wmaxy);
+    // free(wmaxx);
+    // free(lp);
+    // free(i);
+
+}
 
 /**
  * Função que inicia um jogo novo
@@ -17,15 +89,21 @@ void inicia_jogo(WINDOW **janela) {
 
     int fim = 0;
 
+    first_run_jogo(janela[0], new_tab->jog);
+
     do {
 
-        cabecalho_jogo(janela[0], janela[1]);
+        cabecalho_jogo(janela[0], janela[1], new_tab);
 
         imprime_tabuleiro(janela[1], new_tab);
 
         refresh();
 
         switch(get_option()) {
+
+            // Opção de realizar o movimento
+            case '1':
+                realizar_movimento();
 
             // Opção de sair do jogo
             case '0':
